a5c0989062018dd4b56aed37335048003c8adb70
UnrealSpecter <info@dhevak.nl>
2022-11-21 13:53:10 +0100

    created a clear structure for the exercises

---
+++ Level 1 - The fundamentals of Object Oriented Programming in PHP/opdracht 1.1 - What is Object Oriented Programming.php
@@ -0,0 +1,20 @@
+//Opdracht: 1.1 - WHAT IS OOP?
+//Lees op W3Schools de PHP OOP Intro →
+
+//Opdracht 1.1.1
+//Leg in je eigen woorden uit wat het verschil is tussen flat PHP en Object Oriented Programming?
+
+//Opdracht 1.1.2
+//Leg in je eigen woorden uit welke vier voordelen Object Oriented Programming heeft ten opzicht van flat PHP.
+
+//Opdracht 1.1.3
+//Leg in je eigen woorden uit wat de twee hoofdonderdelen zijn van Object Oriented Programming?
+
+//Opdracht 1.1.4
+//Leg in je eigen woorden uit wat het verschil is tussen een Class en een Object?
+
+//Opdracht 1.1.5
+//Leg in je eigen woorden uit wat er overgeërfd wordt van een Class als een object aangemaakt wordt?
+
+//Opdracht 1.1.6
+//Bedenk zelf een voorbeeld van een Class en bijbehorende Objecten anders dan afgebeeld op W3SCHOOLS.
